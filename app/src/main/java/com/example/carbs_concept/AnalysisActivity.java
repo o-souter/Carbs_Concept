499cd428f537b73d2e003cd1a2ed7f57e07b8019
Oliver Souter <73543366+o-souter@users.noreply.github.com>
2025-02-13 10:31:45 +0000

    Communication now established between flask server - images are now uploaded for further processing on backend

--- app/src/main/java/com/example/carbs_concept/AnalysisActivity.java
+++ app/src/main/java/com/example/carbs_concept/AnalysisActivity.java
@@ -6,24 +6,41 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class AnalysisActivity extends AppCompatActivity {
     private String imagePath;
     private ImageView segmentedImgView;
     private Bitmap imageBitmap;
     private ProgressBar progressBar;
+    private String url = "http://192.168.1.168:5000";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,28 +59,110 @@ public class AnalysisActivity extends AppCompatActivity {
             return insets;
         });
 
-        try {
-            sendImageForAnalysis();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        uploadImage(imagePath);
+    }
+
+    private void uploadImage(String imagePath) {
+        OkHttpClient client = new OkHttpClient();
+
+        File imageFile = new File(imagePath);
+        if (!imageFile.exists()) {
+            Log.e("OkHTTP Image Upload", "File not found at " + imagePath);
+            return;
         }
+        //Create request body
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", imageFile.getName(),
+                        RequestBody.create(imageFile, MediaType.parse("image/jpeg")))
+                .build();
+
+        //Create request itself
+        Request request = new Request.Builder()
+                .url(url+"/"+"upload-image")
+                .post(requestBody)
+                .build();
+
+        //Execute request in background
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.e("OkHTTP Image Upload", "Failed: " + e.getMessage());
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.d("OkHTTP Image Upload", "Success"+ response.body().string());
+                }
+                else {
+                    Log.e("OkHTTP Image Upload", "Error"+response.code());
+                }
+            }
+        });
     }
 
     private void sendImageForAnalysis() throws IOException {
         progressBar.setVisibility(View.VISIBLE);
-//        segmentedImgView.setImageBitmap(imageBitmap);
-        //FoodDetectionModel.
-        FoodDetectionModel foodmodel = new FoodDetectionModel(getApplicationContext());//, "yolov2-food100.tflite");
-        YoloInference yoloInference = new YoloInference(foodmodel.getInterpreter());
 
-        float[][][] output = yoloInference.runModel(imageBitmap);
-
-        List<YoloDetectionResult> results = YoloPostProcessor.processOutput(output);
-        Bitmap resultImage = OverlayDrawer.drawDetections(imageBitmap, results);
-        segmentedImgView.setImageBitmap(resultImage);
+        String base64_Img = convertImgToBase64(imageBitmap);
+        sendRequest("POST", "upload-image", "image1", base64_Img);
 
 
         progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void sendRequest(String requestType, String method, String name, String text) {
+        String requestURL = url+"/"+method+"/"+name; //Create the request string e.g. http://192.168.0.0:5000/send-image/base64encdodedimg
+        Request request;
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .build();
+
+        if (requestType == "POST") {
+            //POST request
+            RequestBody formBody = new FormBody.Builder().add(name, text).build();
+
+            request = new Request.Builder().url(requestURL).post(formBody).build();
+        }
+        else {
+            //GET Request
+            request = new Request.Builder().url(requestURL).build();
+        }
+
+        Log.d("AnalysisActivity","Creating request to send data to flask...");
+        Log.d("AnalysisActivity","Sending '" + requestType + "' request to URL: "+requestURL);
+        Log.d("AnalysisActivity","Sending content '" + name + "': "+ text);
+
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+
+                //Read data on worker thread
+                final String responseData = response.body().string();
+
+
+            }
+        });
+    }
+
+    private String convertImgToBase64(Bitmap img) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        img.compress(Bitmap.CompressFormat.PNG, 1, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+
     }
 
     private Bitmap rotateBitmap(Bitmap original, float degrees) {
